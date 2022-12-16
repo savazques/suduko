4fd569dce8de1c9a1da9c187a12d350bb335e844
savaques <savasquez@ucdavis.edu>
2022-12-15 18:36:05 -0800

    created print statements

--- sudoku.cpp
+++ sudoku.cpp
@@ -2,44 +2,20 @@
 #include <vector>
 using namespace std; 
 
-int const N = 9; 
-int const n = 3; 
-
-void printBoard(vector<vector<int> > board)
-{
-    for(int i = 0; i<N; i++)
-    {
-        for(int j = 0; j<N; j++)
-        {
-            cout<<board[i][j]<<endl;
-        }
-    }
-}
-
 
 int main()
 {
-    //intialiizng a matrix to get input of unsolved sudoku
-    vector<vector<int> > board(N); 
+    int difficulty; 
 
-    for (int i=0; i<N; i++)
-    {
-        for (int k = 0; k < N; k++)
-        {
-            int game;
-            cin>>game; 
-            if(game>=0 && game<=N)
-            {
-                board[i].push_back(game); 
-            }    
-            else
-            {
-                    cout<<"Not Valid";                
-            }
-            
-        }
-    }
-   printBoard(board);  
+    cout<<"Welcome to Suduko"<<"\n"<<
+    "Select Difficulty"<<"\n"<<
+    "1. Easy"<<"\n"<<
+    "2. Medium"<<"\n"<<
+    "3. Hard"<<"\n"<<
+    "Enter 1 2 or 3: "<<endl;
+
+    cin>>difficulty;
+
 
     return 0; 
 }
