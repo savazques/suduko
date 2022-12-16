c66a44f647d56fc1c8dee852fc6b395b3350449c
savaques <savasquez@ucdavis.edu>
2022-12-15 19:03:16 -0800

    skeleton of methods and constructors

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,3 +1,28 @@
 #include  "sudoku.h"
 #include <iostream>
-#include <cstdlib>
+#include <cstdlib> 
+
+
+//defining constructors
+
+    Sudoku::Sudoku()
+    {
+        this->level = level;
+    }
+
+    Sudoku::Sudoku(int level)
+    {
+        this -> level = level;
+    } 
+
+//defining methods
+
+    void printBoard()
+    {
+
+    }
+
+    void solveBoard()
+    {
+        
+    }
--- sudoku.h
+++ sudoku.h
@@ -2,5 +2,31 @@
 #define SUDOKU_
 
 #include<iostream>
+using namespace std; 
+
+class Sudoku
+{
+private:
+
+//attributes
+    int row = 9; 
+
+    int column = 9; 
+
+public:
+
+int level; 
+
+//constructors
+    Sudoku();
+    
+    Sudoku(int level); 
+
+//methods
+    void printBoard();
+
+    void solveBoard();
+
+};
 
 #endif
